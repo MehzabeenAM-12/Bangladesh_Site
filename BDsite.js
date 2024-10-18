5fc7bb72de77c7dc3e023cc15263565b909b0c60
MehzabeenAM-12 <mehzabeenmahfuz@gmail.com>
2024-10-18 01:52:12 +0100

    initial commit

---
+++ BDsite.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById("green");
+btn.addEventListener("click", () => {
+   document.body.style.background = "#8FBC8F";
+});                
+
+let golapi = document.getElementById("pink");
+golapi.addEventListener("click", () => {
+   document.body.style.background = "#FFB6C1";
+});                
+
+let neel = document.getElementById("blue");
+neel.addEventListener("click", () => {
+   document.body.style.background = "#ADD8E6";
+});  
+
+let beguni = document.getElementById("lavender");
+beguni.addEventListener("click", () => {
+   document.body.style.background = "#E6E6FA";
+}); 
+
+let beij = document.getElementById("beige");
+beij.addEventListener("click", () => {
+   document.body.style.background = "beige";
+}); 
+
+let shada = document.getElementById("white");
+shada.addEventListener("click", () => {
+   document.body.style.background = "white";
+}); 
+
